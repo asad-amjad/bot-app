dcf3e7980a2c04f69277bc1c76c90dbb9c443b99
Asad <asad.amjad.pk@gmail.com>
2024-08-29 14:12:52 +0500

    rfp logo

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,30 +1,20 @@
 import { useState } from "react";
 import { marked } from "marked";
 import { markdownContent } from "../../markdownData";
-import TopNavbar from "../../components/navbar/TopNavbar";
-import {
-  // Button,
-  Card,
-  CardBody,
-  // CardFooter,
-  // CardHeader,
-  Container,
-} from "react-bootstrap";
+import { Card, CardBody, Container } from "react-bootstrap";
 import LeftPanel from "../../components/LeftPanel/LeftPanel";
 import styles from "./Dashboard.module.css";
 import { FiDownload } from "react-icons/fi";
 import { FiCopy } from "react-icons/fi";
 import Tooltip from "../../components/tooltip/Tooltip";
+import rfpLogo from "../../assets/rfp.png";
 
 const Dashboard = () => {
-  // const [query, setQuery] = useState("");
   const [response, setResponse] = useState("");
   const [loading, setLoading] = useState(false);
 
   const handleFileUpload = (files) => {
-    // Handle the array of files here
     console.log(files);
-    // You can process the files or make an API call here
   };
 
   const handleQuerySubmit = () => {
@@ -37,46 +27,59 @@ const Dashboard = () => {
   };
 
   return (
-   
-    
-      <Container className={styles.container}>
-        <LeftPanel
-          handleQuerySubmit={handleQuerySubmit}
-          handleFileUpload={handleFileUpload}
-          loading={loading}
-        />
-        <Card className={`col-md-9 ${styles.rightPanel} shadow `}>
-          <CardBody>
-            <div className={`${styles.contentHeader}`}>
-              <Tooltip content="Copy content" place="bottom">
-                <div className="card p-2" role="button">
-                  <FiCopy />
-                </div>
-              </Tooltip>
+    <Container className={styles.container}>
+      <LeftPanel
+        handleQuerySubmit={handleQuerySubmit}
+        handleFileUpload={handleFileUpload}
+        loading={loading}
+      />
+      <Card className={`col-md-9 ${styles.rightPanel} shadow`}>
+        <CardBody className={styles.cardBody}>
+          {response && (
+            <div className={styles.contentHeader}>
+              <img src={rfpLogo} alt="RFP logo" width={100} />
+              <div className="d-flex align-items-center gap-2">
+                <Tooltip content="Copy content" place="bottom">
+                  <div className="card p-2" role="button">
+                    <FiCopy />
+                  </div>
+                </Tooltip>
 
-              <Tooltip content="Download file" place="bottom" >
-                <div className="card p-2" role="button">
-                  <FiDownload />
-                </div>
-              </Tooltip>
-            </div>
-
-            {loading ? (
-              <div className="d-flex justify-content-center align-items-center h-100">
-                <div className="spinner-border" role="status">
-                  <span className="visually-hidden">Loading...</span>
-                </div>
+                <Tooltip content="Download file" place="bottom">
+                  <div className="card p-2" role="button">
+                    <FiDownload />
+                  </div>
+                </Tooltip>
               </div>
-            ) : (
-              <div
-                dangerouslySetInnerHTML={{ __html: marked(response) }}
-                className={styles.markdownContent}
-              />
-            )}
-          </CardBody>
-        </Card>
-      </Container>
+            </div>
+          )}
 
+          {!response && !loading && (
+            <div className="d-flex justify-content-center flex-column align-items-center h-75">
+              <img
+                src={rfpLogo}
+                className={styles.rfplogo}
+                alt="RFP logo"
+                width={350}
+              />
+            </div>
+          )}
+
+          {loading ? (
+            <div className="d-flex justify-content-center align-items-center h-75">
+              <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div>
+            </div>
+          ) : (
+            <div
+              dangerouslySetInnerHTML={{ __html: marked(response) }}
+              className={styles.markdownContent}
+            />
+          )}
+        </CardBody>
+      </Card>
+    </Container>
   );
 };
 
