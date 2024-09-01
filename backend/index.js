23011883c35e2758adf74abd13ef71141c306e60
Asad <asad.amjad.pk@gmail.com>
2024-09-02 02:21:35 +0500

    url

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // app.use(cors());
 
 app.use(cors({
-  origin: ['http://localhost:5173/', 'http://localhost:5173'],
+  origin: ['http://localhost:5173/', 'http://localhost:5173', "https://preeminent-kelpie-d4b81f.netlify.app"],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
